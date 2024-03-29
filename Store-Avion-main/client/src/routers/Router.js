5a69812d4f3a4215e8b7d46152f892ba2978adb9
Volodymyr Medvedchuk <vladmeeedy@gmail.com>
2024-03-29 15:44:10 +0200

    add

--- Store-Avion-main/client/src/routers/Router.js
+++ Store-Avion-main/client/src/routers/Router.js
@@ -1,31 +1,40 @@
 import React from 'react'
 import { Navigate, useRoutes, BrowserRouter } from 'react-router-dom'
 
-import HomePage from '../pages/HomePage/HomePage'
-import Layout from '../layouts/Layout'
-
 const HomePage = React.lazy(
   () => import(/* webpackChunkName: "HomePage" */ '../pages/HomePage/HomePage'),
 )
 const Layout = React.lazy(
-  () => import(/* webpackChunkName: "Layout" */ 'Layout'),
+  () => import(/* webpackChunkName: "Layout" */ '../layouts/Layout'),
 )
 
 const CartPage = React.lazy(
   () => import(/* webpackChunkName: "CartPage" */ '../pages/CartPage/index'),
 )
 const FavoritesPage = React.lazy(
-  () => import(/* webpackChunkName: "FavoritesPage" */ '../pages/FavoritesPage/FavoritesPage'),
+  () =>
+    import(
+      /* webpackChunkName: "FavoritesPage" */ '../pages/FavoritesPage/FavoritesPage'
+    ),
 )
 const CategoryPage = React.lazy(
-  () => import(/* webpackChunkName: "CategoryPage" */ '../pages/CategoryPage/CategoryPage'),
+  () =>
+    import(
+      /* webpackChunkName: "CategoryPage" */ '../pages/CategoryPage/CategoryPage'
+    ),
 )
 const ProductPage = React.lazy(
-  () => import(/* webpackChunkName: "ProductPage" */ '../pages/ProductPage/ProductPage'),
+  () =>
+    import(
+      /* webpackChunkName: "ProductPage" */ '../pages/ProductPage/ProductPage'
+    ),
 )
 
 const AllProductsPage = React.lazy(
-  () => import(/* webpackChunkName: "AllProductsPage" */ '../pages/AllProductsPage/AllProductsPage.js'),
+  () =>
+    import(
+      /* webpackChunkName: "AllProductsPage" */ '../pages/AllProductsPage/AllProductsPage.js'
+    ),
 )
 
 const AuthPage = React.lazy(
@@ -33,27 +42,38 @@ const AuthPage = React.lazy(
 )
 
 const OrderPage = React.lazy(
-  () => import(/* webpackChunkName: "OrderPage" */ '../pages/OrderPage/OrderPage'),
+  () =>
+    import(/* webpackChunkName: "OrderPage" */ '../pages/OrderPage/OrderPage'),
 )
 const AboutPage = React.lazy(
-  () => import(/* webpackChunkName: "AboutPage" */ '../pages/AboutPage/AboutPage'),
+  () =>
+    import(/* webpackChunkName: "AboutPage" */ '../pages/AboutPage/AboutPage'),
 )
 const PageNotFound = React.lazy(
-  () => import(/* webpackChunkName: "PageNotFound" */ '../pages/PageNotFound/PageNotFound'),
+  () =>
+    import(
+      /* webpackChunkName: "PageNotFound" */ '../pages/PageNotFound/PageNotFound'
+    ),
 )
 const PrivacyPage = React.lazy(
-  () => import(/* webpackChunkName: "PrivacyPage" */ '../pages/PrivacyPage/PrivacyPage'),
+  () =>
+    import(
+      /* webpackChunkName: "PrivacyPage" */ '../pages/PrivacyPage/PrivacyPage'
+    ),
 )
 const ScrollToTop = React.lazy(
   () => import(/* webpackChunkName: "ScrollToTop" */ './ScrollToTop'),
 )
 const ProfilePage = React.lazy(
-  () => import(/* webpackChunkName: "ProfilePage" */ '../pages/ProfilePage/ProfilePage'),
+  () =>
+    import(
+      /* webpackChunkName: "ProfilePage" */ '../pages/ProfilePage/ProfilePage'
+    ),
 )
 const ContactUs = React.lazy(
-  () => import(/* webpackChunkName: "ContactUs" */ '../pages/ContacUs/index.js'),
+  () =>
+    import(/* webpackChunkName: "ContactUs" */ '../pages/ContacUs/index.js'),
 )
-
 
 function Routes() {
   return useRoutes([
@@ -82,11 +102,11 @@ function Routes() {
 
 export default function Router() {
   return (
-    <BrowserRouter>      
+    <BrowserRouter>
       <React.Suspense fallback={<div>Loading...</div>}>
         <ScrollToTop />
         <Routes />
-      </React.Suspense >      
+      </React.Suspense>
     </BrowserRouter>
   )
 }
