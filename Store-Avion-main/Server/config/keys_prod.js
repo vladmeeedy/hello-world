eb2df1ebc21272a4917a2dc72d14667ceedc3424
Volodymyr Medvedchuk <vladmeeedy@gmail.com>
2024-04-01 10:27:26 +0300

    changed pdf

--- Store-Avion-main/Server/config/keys_prod.js
+++ Store-Avion-main/Server/config/keys_prod.js
@@ -5,3 +5,16 @@ module.exports = {
   nodemailerPassword: process.env.NODEMAILER_PASSWORD,
   nodemailerService: process.env.NODEMAILER_SERVICE
 };
+import { ContentfulContentSource } from "@stackbit/cms-contentful";
+
+export default {
+  contentSources: [
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN
+    })
+  ]
+  // ...
+};
